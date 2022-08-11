82e5df6f24f2c0f71744309497735cf82a6ae696
stefanussuwandi08 <stefanussuwandi08@gmail.com>
2022-08-11 23:22:14 +0700

    1. add textfield email & password 2. add obscure function 3. add forgot password and remember me

--- lib/controller/login controller/login_contorller.dart
+++ lib/controller/login controller/login_contorller.dart
@@ -1,8 +1,10 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 class LoginController extends GetxController {
   var isObscure = true.obs;
+  RxBool isChecked = false.obs;
   final GlobalKey<FormState> formKey = GlobalKey();
   final email_controller = TextEditingController();
   final password_controller = TextEditingController();
@@ -10,5 +12,32 @@ class LoginController extends GetxController {
   @override
   void dispose() {
     super.dispose();
+  }
+
+  void handleRemeberme(bool value) {
+    isChecked.value = value;
+    SharedPreferences.getInstance().then(
+      (prefs) {
+        prefs.setBool("remember_me", value);
+        prefs.setString('email', email_controller.text);
+        prefs.setString('password', password_controller.text);
+      },
+    );
+  }
+
+  void loadUserEmailPassword() async {
+    try {
+      SharedPreferences _prefs = await SharedPreferences.getInstance();
+      var _email = _prefs.getString("email") ?? "";
+      var _password = _prefs.getString("password") ?? "";
+      var _remeberMe = _prefs.getBool("remember_me") ?? false;
+      if (_remeberMe) {
+        isChecked.value = true;
+        email_controller.text = _email ?? "";
+        password_controller.text = _password ?? "";
+      }
+    } catch (e) {
+      print(e);
+    }
   }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
-
+import 'package:flutter_gradient_colors/flutter_gradient_colors.dart';
 import 'controller/login controller/login_contorller.dart';
 
 void main() {
@@ -17,64 +17,109 @@ class loginScreen extends StatelessWidget {
   var controller = Get.put(LoginController());
   @override
   Widget build(BuildContext context) {
-    return Obx(() => MaterialApp(
-            home: Scaffold(
-          appBar: AppBar(
-            title: Text('Template Apps'),
-          ),
-          body: Padding(
-            padding: const EdgeInsets.all(16),
-            child: Center(
-                child: Form(
-              key: controller.formKey,
-              child: Column(
-                children: [
-                  Text(
-                    'Hello Again!',
-                    style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
-                  ),
-                  Padding(
-                    padding: const EdgeInsets.all(18.0),
-                    child: Text('Welcome back, you have been missed!'),
-                  ),
-                  TextField(
-                    decoration: InputDecoration(
-                        hintText: 'Email',
-                        labelText: 'Email',
-                        border: OutlineInputBorder(
-                            borderRadius: BorderRadius.circular(10),
-                            borderSide: BorderSide(color: Colors.black))),
-                    controller: controller.email_controller,
-                  ),
-                  SizedBox(
-                    height: 15,
-                  ),
-                  TextField(
-                    obscureText: controller.isObscure.value,
-                    decoration: InputDecoration(
-                        hintText: 'Password',
-                        labelText: 'Password',
-                        suffixIcon: IconButton(
-                          icon: Icon(
-                            (controller.isObscure.value)
-                                ? Icons.visibility
-                                : Icons.visibility_off,
+    return Obx(() => Scaffold(
+          body: Stack(children: [
+            Container(
+                height: double.infinity,
+                width: double.infinity,
+                decoration: BoxDecoration(
+                    gradient: LinearGradient(
+                        begin: Alignment.topCenter,
+                        end: Alignment.bottomRight,
+                        colors: GradientColors.confidentCloud))),
+            Container(
+              child: Padding(
+                padding: const EdgeInsets.all(16),
+                child: Center(
+                    child: Form(
+                  key: controller.formKey,
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Text(
+                        'Hello Again!',
+                        style: TextStyle(
+                            fontSize: 18, fontWeight: FontWeight.bold),
+                      ),
+                      Padding(
+                        padding: const EdgeInsets.all(18.0),
+                        child: Text('Welcome back, you have been missed!'),
+                      ),
+                      TextField(
+                        decoration: InputDecoration(
+                            suffixIcon: IconButton(
+                              icon: Icon(Icons.email),
+                              onPressed: () {},
+                            ),
+                            hintText: 'Email',
+                            labelText: 'Email',
+                            border: OutlineInputBorder(
+                                borderRadius: BorderRadius.circular(10),
+                                borderSide: BorderSide(color: Colors.black))),
+                        controller: controller.email_controller,
+                      ),
+                      SizedBox(
+                        height: 15,
+                      ),
+                      TextField(
+                        obscureText: controller.isObscure.value,
+                        decoration: InputDecoration(
+                            hintText: 'Password',
+                            labelText: 'Password',
+                            suffixIcon: IconButton(
+                              icon: Icon(
+                                (controller.isObscure.value)
+                                    ? Icons.visibility
+                                    : Icons.visibility_off,
+                              ),
+                              onPressed: () {
+                                (controller.isObscure.value)
+                                    ? controller.isObscure.value = false
+                                    : controller.isObscure.value = true;
+                              },
+                            ),
+                            border: OutlineInputBorder(
+                                borderRadius: BorderRadius.circular(10),
+                                borderSide: BorderSide(color: Colors.black))),
+                        controller: controller.password_controller,
+                      ),
+                      SizedBox(
+                        height: 10,
+                      ),
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.end,
+                        children: [
+                          InkWell(
+                            onTap: () {},
+                            child: Text(
+                              'Forgot Password?',
+                              style:
+                                  TextStyle(color: Colors.black, fontSize: 12),
+                            ),
                           ),
-                          onPressed: () {
-                            (controller.isObscure.value)
-                                ? controller.isObscure.value = false
-                                : controller.isObscure.value = true;
-                          },
-                        ),
-                        border: OutlineInputBorder(
-                            borderRadius: BorderRadius.circular(10),
-                            borderSide: BorderSide(color: Colors.black))),
-                    controller: controller.password_controller,
+                        ],
+                      ),
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.start,
+                        children: [
+                          Checkbox(
+                              value: controller.isChecked.value,
+                              onChanged: (context) {
+                                controller.handleRemeberme(
+                                    controller.isChecked.value);
+                              }),
+                          Text(
+                            'Remember Me',
+                            style: TextStyle(fontSize: 11),
+                          )
+                        ],
+                      ),
+                    ],
                   ),
-                ],
+                )),
               ),
-            )),
-          ),
-        )));
+            ),
+          ]),
+        ));
   }
 }
